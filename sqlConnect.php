ffb7ce3b99897a2ac68d9fd4eb1cee690068bf92
solomon234 <solomonmuratov@gmail.com>
2018-11-13 14:27:25 -0500

    Revert "for github pages"
    This reverts commit 9730f6edf73942a41db23c3630631a1fd627bee6.

--- sqlConnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Connect to MySQL and the Database
-  $mysqli = new mysqli('localhost', 'root', '', 'test1');
-  if ($mysqli->connect_error)
-    die('Connect Error: ' . $mysqli->connect_error);	
-?>  
